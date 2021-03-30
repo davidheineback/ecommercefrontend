42c4ae47c6e5348f8342293a41d9a8b387ec763a
David Heinebäck <david@AirsomthorDavid.lan>
2021-03-30 15:08:08 +0200

    add: slugify and Link to route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
+import slugify from 'slugify'
+import { Link } from 'react-router-dom'
 import Button from '../Utilities/Button.js'
 
 const StyledHeader = styled.div`
 position: fixed;
 top: 0;
 width: 100%;
-background-color: ${props => props.theme.headerColors.main};
+background-color: ${props => props.theme.colors.headerBackground};
 margin: 0;
 left: 0;
-padding: 3rem 1rem;
+padding: 4rem 1rem;
+padding-bottom: 5px;
 text-align: center;
 `
+
+const categories = ["First", "Second", "Third"]
 
 function Header( { onScroll } ) {
   return (
     <StyledHeader>
-      <Button>Hej</Button>
-      <Button>Branch</Button>
-      <Button primary>Primary hej</Button>
+      {categories.map((category, index) => {
+        return (
+          <Link to={'/'+ slugify(category, {
+            lower: true
+          })}>
+          <Button
+            btnType="headerBtn"
+            key={index}>
+              {category}
+          </Button>
+          </Link>
+        )
+      })}
     </StyledHeader> 
   )
 }
