51542f6284c091e67dfb5adf2658e5761bd56866
David Heinebäck <david@AirsomthorDavid.lan>
2021-03-30 10:31:06 +0200

    add styled components and styling to Button utility

--- src/components/Utilities/Button.js
+++ src/components/Utilities/Button.js
@@ -1,15 +1,28 @@
 import styled from 'styled-components'
 import React from 'react'
 
-const button = styled.button`
-
+const StyledButton = styled.button`
+background: ${props => props.primary ? "black" : "white"};
+color: ${props => props.primary ? "white" : "black"};
+appearance: none;
+border: 0;
+border-radius: 100px;
+box-shadow: none;
+cursor: pointer;
+font-weight: 700;
+letter-spacing: 2px;
+margin: 1rem 2px 5px 2px;
+padding: 1rem 1rem;
+text-transform: uppercase;
+&:hover {
+background: ${props => !props.primary ? "black" : "white"};
+color: ${props => !props.primary ? "white" : "black"};
+}
 `
 
-function Button() {
+function Button({ btnType, children }) {
   return (
-    <div>
-      
-    </div>
+    <StyledButton btnType={btnType}>{children}</StyledButton>
   )
 }
 
