8d39bc3497e4a585949528a255e5d97f5fb99b70
David Heinebäck <david@AirsomthorDavid.lan>
2021-03-30 10:32:04 +0200

    add styled components and styling to themeDiv utility

--- src/components/Utilities/ThemeDiv.js
+++ src/components/Utilities/ThemeDiv.js
@@ -1,12 +1,17 @@
 import styled from 'styled-components'
-
 import React from 'react'
 
-function ThemeDiv() {
+const WrapperDiv = styled.div `
+color: red;
+margin: 20px;
+position: relative;
+`
+
+function ThemeDiv({ children }) {
   return (
-    <div>
-      
-    </div>
+    <WrapperDiv>
+      {children}
+    </WrapperDiv>
   )
 }
 
