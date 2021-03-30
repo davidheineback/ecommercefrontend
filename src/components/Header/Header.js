0a4b1c78d43d19c52920059ea87c9a94a661854a
David Heinebäck <david@AirsomthorDavid.lan>
2021-03-30 10:31:47 +0200

    add styled components and styling to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,23 @@
-import styled from 'styled-components'
 import React from 'react'
+import styled from 'styled-components'
+import Button from '../Utilities/Button.js'
 
-function Header() {
+const StyledHeader = styled.div`
+position: fixed;
+top: 0;
+width: 100%;
+background-color: red;
+margin: 0;
+left: 0;
+padding: 3rem 1rem;
+`
+
+function Header( { onScroll } ) {
   return (
-    <div>
-      
-    </div>
+    <StyledHeader>
+      <Button>Hej</Button>
+      <Button primary>Primary hej</Button>
+    </StyledHeader> 
   )
 }
 
