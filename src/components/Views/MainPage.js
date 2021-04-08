e8a1638d15071209e5da568ac046e2eba65169d8
David Heinebäck <du222aa@student.lnu.se>
2021-04-08 20:44:37 +0200

    refactor to fetch from backend instead of mockDB

--- src/components/Views/MainPage.js
+++ src/components/Views/MainPage.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Wrapper from '../Utilities/Wrapper.js'
 import ProductCard from '../Product/ProductCard.js'
-import products from '../../mockDB/mockProducts.js'
+import { getAllProducts } from '../../fetch.js'
 
 
 function MainPage() {
+  const [products, setProducts] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  
+  useEffect(() => {
+    setIsLoading(true)
+    getAllProducts(setProducts, setIsLoading) },[])
+    if (isLoading) {
+      return <Wrapper flex/>
+    }
   return (
     <Wrapper flex>
     {products.map((product, index) => {
