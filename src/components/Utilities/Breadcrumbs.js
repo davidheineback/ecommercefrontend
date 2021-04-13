ece906cbc5f8d110a03be31d4d859432b832d419
David Heinebäck <du222aa@student.lnu.se>
2021-04-13 14:53:04 +0200

    add breadcrumbs

--- src/components/Utilities/Breadcrumbs.js
+++ src/components/Utilities/Breadcrumbs.js
@@ -8,30 +8,64 @@ display: flex;
 justify-content: left;
 background-color: white;
 height: 15px;
-
 `
+
+const StyledTilter = styled.div`
+transform: skewX(-40deg);
+height: 20px;
+padding-right: 5px;
+color: ${props => props.theme.fontColors.dark};
+&:hover {
+background: ${props => props.theme.colors.headerBackground};
+color: ${props => props.theme.fontColors.mainHover};
+box-shadow: 2px 2px 2px ${props => props.theme.colors.main};
+}
+
+${props => props.active && {
+background: props.theme.colors.main,
+boxShadow: "2px 2px 2px black",
+color: props.theme.fontColors.mainHover,
+}}
+`
+
+
 const StyledBreadcrumbs = styled(Link)`
 font-size: 10px;
 margin-left: 10px;
-text-decoration: none;
-color: ${props => props.theme.fontColors.dark};
 cursor: pointer;
 display: inline-block;
+color: inherit;
+text-decoration: none;
 font-weight: 700;
 text-transform: uppercase;
+transform: skewX(40deg);
+&:hover {
+background: ${props => props.theme.colors.headerBackground};
+color: ${props => props.theme.fontColors.mainHover};
+}
 `
 
 
 function Breadcrumbs({ children }) {
   return (
     <StyledBreadcrumbsContainer>
-      <StyledBreadcrumbs
+      <StyledTilter>
+        <StyledBreadcrumbs
       key={'start'}
       to='/'
-      style={{ textDecoration: 'none' }}>Start</StyledBreadcrumbs>
+      style={{ textDecoration: 'none' }}>
+        Start
+        </StyledBreadcrumbs>
+      </StyledTilter>
       {children
       .filter(child => child !== undefined)
-      .map((child, index) => <StyledBreadcrumbs key={index} to={`/${child}`}>{` > ${child}`}</StyledBreadcrumbs>)}
+      .map((child, index) => {
+        return (
+          <StyledTilter key={index} active={index === (children.length - 1)}>
+            <StyledBreadcrumbs key={index} to={`/${child}`}>{` > ${child}`}</StyledBreadcrumbs>
+            </StyledTilter>
+        )
+      })}
      
     </StyledBreadcrumbsContainer>
   )
