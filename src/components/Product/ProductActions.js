fd12615b4dd4eb25bd7549a98fc133370de09b34
David Heinebäck <du222aa@student.lnu.se>
2021-04-14 16:22:46 +0200

    add: productActions componenet

--- src/components/Product/ProductActions.js
+++ src/components/Product/ProductActions.js
@@ -3,21 +3,36 @@ import styled from 'styled-components'
 import { Button } from '../Utilities/UtilitiesExporter'
 
 const StyledProductAction = styled.div`
+display: block;
 border: 2px solid black;
 padding: 10px;
 margin: 10px;
+width: 80%;
 text-align: center;
+`
+
+const StyledSelect = styled.select`
+border: 0;
+border-bottom: 2px solid ${props => props.theme.colors.main};
+cursor: pointer;
+font-weight: 700;
+letter-spacing: 2px;
+min-width: 40%;
+padding: 0.5rem 0.5rem;
+`
+
+const StyledOption = styled.option`
 `
 
 
 function ProductActions({ children }) {
   return (
     <StyledProductAction>
-      {children && children.options.length > 0 && 
-      <select>
-      {children.options.map(child => <option>{child}</option>
+      {children.options && children.options.length > 0 && 
+      <StyledSelect>
+      {children.options.map((child, index) => <StyledOption val={index}>{child}</StyledOption>
       )}
-      </select>
+      </StyledSelect>
       }
       <Button btnType={'primary'}>Buy me now!</Button>
     </StyledProductAction>
