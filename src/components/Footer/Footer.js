976bac8d3dc2baed1640753a1db555f30b4e56c5
David Heinebäck <du222aa@student.lnu.se>
2021-04-19 09:53:57 +0200

    refactor: add github link to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,20 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledFooter = styled.div`
+right: 0;
+left: 0;
 bottom: 0;
-max-height: 40px;
-width: 100vw;
-margin-right: 0;
 background-color: ${props => props.theme.colors.footerBackground};
 border-top: 3px solid ${props => props.theme.colors.main};
-padding: 2.5rem 1rem;
+padding: 2.2rem 1rem;
 text-align: ${props => props.align || "center"};
+text-decoration: none;
+color: white;
+font-size: 12px;
+overflow: hidden;
+`
+
+const StyledLink = styled.a`
+text-decoration: underline;
+color: white;
+font-size: 12px;
 `
 
 function Footer({ children }) {
   return (
     <StyledFooter>
       {children}
+    <StyledLink target="_blank" href="https://github.com/davidheineback/ecommercefrontend">Visit github for code</StyledLink>  
     </StyledFooter>
   )
 }
