56920456ac82db01f31984579b35e253462a1837
David Heinebäck <du222aa@student.lnu.se>
2021-04-19 09:58:11 +0200

    add: Flash component to views

--- src/components/Views/ProductPage.js
+++ src/components/Views/ProductPage.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { Flash } from './ViewsExporter'
 import { Breadcrumbs, Wrapper, ProductImage, ProductPageDetails } from '../Utilities/UtilitiesExporter'
 import ProductActions from '../Product/ProductActions'
 import { getProductById } from '../../fetch.js'
+
 
 function ProductPage({ size }) {
   const { mainCategory, subCategory, productId } = useParams()
@@ -14,6 +16,7 @@ function ProductPage({ size }) {
   return (
     <Wrapper flex='bigFlex'>
       <Breadcrumbs itemNr={productId}>{[mainCategory, subCategory, product.name]}</Breadcrumbs>
+      <Flash/>
       <ProductImage url={product.image} description={product.description} type='productPage'/>
       <Wrapper size={size} productDetails>
       <ProductPageDetails>{product}</ProductPageDetails>
