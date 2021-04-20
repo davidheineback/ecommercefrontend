a060c6188f3a520037e0005ab8c3a402715af9b8
David Heinebäck <du222aa@student.lnu.se>
2021-04-20 17:00:13 +0200

    refactor: add awaits

--- src/fetch.js
+++ src/fetch.js
@@ -4,7 +4,7 @@ export async function getCategories(setCategories) {
   try {
     const categories = await fetch(`${process.env.REACT_APP_URL}/category/`)
     const response = await categories.json()
-    setCategories(response)
+    setCategories(await response)
   } catch (error) {
     throw new Error(error)
   }
@@ -14,7 +14,7 @@ export async function getAllProducts(setProducts) {
   try {
     const products = await fetch(`${process.env.REACT_APP_URL}/products/`)
     const response = await products.json()
-    setProducts(response)
+    setProducts(await response)
   } catch (error) {
     throw new Error(error)
   }
@@ -24,7 +24,7 @@ export async function getProductsByCategory(category, setProducts) {
   try {
     const products = await fetch(`${process.env.REACT_APP_URL}/category/${category}`)
     const response = await products.json()
-    setProducts(response)
+    setProducts(await response)
   } catch (error) {
     throw new Error(error)
   }
@@ -34,7 +34,7 @@ export async function getProductsBySubCategory(subcategory, setProducts) {
   try {
     const products = await fetch(`${process.env.REACT_APP_URL}/subcategory/${subcategory}`)
     const response = await products.json()
-    setProducts(response)
+    setProducts(await response)
   } catch (error) {
     throw new Error(error)
   }
@@ -44,7 +44,7 @@ export async function getProductById(productId, setProduct) {
   try {
     const products = await fetch(`${process.env.REACT_APP_URL}/product/${productId}`)
     const response = await products.json()
-    setProduct(response)
+    setProduct(await response)
   } catch (error) {
     throw new Error(error)
   }
