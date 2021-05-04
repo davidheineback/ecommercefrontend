8a078c2074c82a9b944d86d65cbf7bf27fa45e7b
David Heinebäck <du222aa@student.lnu.se>
2021-05-04 13:59:02 +0200

    refactor: add styling to GradietBorder component

--- src/components/Utilities/GradientBorder.js
+++ src/components/Utilities/GradientBorder.js
@@ -2,15 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledGradientBorder = styled.div`
-
+  height: 5px;
+  width: 100%;
+  background: ${props => props.theme.colors.borderGradient};
 `
 
 
 function GradientBorder() {
   return (
-    <StyledGradientBorder>
-      
-    </StyledGradientBorder>
+    <StyledGradientBorder/>
   )
 }
 
