06707c2769db4a741ffccdee1799bd9d9e496e39
David Heinebäck <du222aa@student.lnu.se>
2021-05-07 14:14:00 +0200

    import render react-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import GlobalStyle from './themes/GlobalStyle.js'
 import Theme from './themes/Theme.js'
 import { ThemeProvider } from 'styled-components'
 import App from './App'
 
-ReactDOM.render(
+render(
   <ThemeProvider theme={Theme}>
     <GlobalStyle/>
     <App />
