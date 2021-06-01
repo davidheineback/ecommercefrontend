e3f2a5b24102641a2cf134dfc406a9acbfea3fb4
David Heinebäck <du222aa@student.lnu.se>
2021-06-01 14:46:06 +0200

    add linter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
