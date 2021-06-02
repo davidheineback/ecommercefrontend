2e4602474b767f1a2e5ea5b65564723f1ba16e5e
David Heinebäck <du222aa@student.lnu.se>
2021-06-02 10:14:43 +0200

    refactor: add styled link to sidebar

--- src/components/Utilities/Sidebar/Sidebar.js
+++ src/components/Utilities/Sidebar/Sidebar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyledSideBar } from './SidebarStyles'
+import { StyledLink } from '../../Header/StyledHeader'
 import { DivButton } from '../UtilitiesExporter'
 
 
@@ -8,14 +9,16 @@ function SideBar({ categories }) {
     <StyledSideBar small>
     {categories.map((category, index) => {
             return (  
+            <StyledLink key={index} to={`./${category.searchurl}`} >
             <DivButton
             sideBar={true}
-            useDropdown={true}
+            useDropdown={false}
             btnType="headerBtn"
             mainCategory={category}
             subCategories={category.subs}
             key={'headerbtn'+index}>
             </DivButton>
+            </StyledLink>
             )
       })}
     </StyledSideBar>
